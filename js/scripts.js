dc9262dd7c42da457c1dd90ce2398aeb93eddcf2
brandon <bcooper085@gmail.com>
2017-01-13 15:17:11 -0800

    add additional conditional options

--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,18 @@ $(function() {
 
     if ($('option:selected').val() === "selected") {
       var result = "Please complete the questions to see your recommended language.";
+    } else if (selected2 === "java" && selected4 === "c#") {
+      var result = "You should try C# or Java!";
     } else if (selected4 === "c#" || selected5 === "c#" && selected6 !== "design") {
       var result = "You should be a C#/.NET developer!";
-    } else if (selected1 === "ruby" || selected2 === "ruby" || selected5 === "ruby" && selected6 !== "design") {
-      var result = "You should be a Ruby/Rails developer!";
     } else if (selected1 === "java" && selected6 !== "design") {
       var result = "You should be a Java/Android developer!";
     } else if (selected3 === "php" && selected6 !== "design") {
       var result = "You should be a PHP/Drupal developer!";
     } else if (selected6 === "design") {
       var result = "You should study CSS/Design!";
+    } else if (selected1 === "ruby" || selected2 === "ruby" || selected5 === "ruby" &&  selected6 !== "design") {
+      var result = "You should be a Ruby/Rails developer!";
     } else if (selected6 === "x" && selected5 === "x" && selected4 === "x" && selected3 === "x" && selected2 === "x" && selected1 === "x") {
       var result = "Maybe this isn't for you!";
     }
