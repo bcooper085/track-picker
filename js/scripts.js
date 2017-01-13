0fde75f927f0cdd95eca89b68d1888308dded529
brandon <bcooper085@gmail.com>
2017-01-13 12:45:48 -0800

    add possible answer combinations

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,18 @@ $(function() {
     var selected3 = $('#question3 option:selected');
     var selected4 = $('#question4 option:selected');
     var selected5 = $('#question5 option:selected');
-    if (selected1.val() === "c#" ) {
+    if (selected1.val() === "c#" || selected4.val() === "c#") {
       var result = "You should be a C#/.NET developer!";
-    } else if (selected1.val() === "ruby" ) {
+    } else if (selected1.val() === "ruby" || selected5.val() === "ruby" ) {
       var result = "You should be a Ruby/Rails developer!";
-    } else if (selected1.val() === "java" ) {
+    } else if (selected1.val() === "java" || selected2.val() === "java") {
       var result = "You should be a Java/Android developer!";
-    } else if (selected1.val() === "php" ) {
+    } else if (selected1.val() === "php" || selected3.val() === "php") {
       var result = "You should be a PHP/Drupal developer!";
-    } else if (selected1.val() === "design" ) {
+    } else if (selected1.val() === "design" || selected6.val() === "design") {
       var result = "You should study CSS/Design!";
+    } else if (selected1.val() === "x" ) {
+      var result = "Maybe this isn't for you!";
     } else {
       var result = "Please complete the questions to see your recommended language."
     }
