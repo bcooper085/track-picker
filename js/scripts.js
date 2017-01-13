3149cdb8b474f757fa2c3255f58972049cfd1f1a
brandon <bcooper085@gmail.com>
2017-01-13 12:23:22 -0800

    add css question and background styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
 
 $(function() {
   $("form").submit(function() {
-
-    if ( $('#answer1 option:selected').val() === "c#" ) {
-      var result = "You should be a C# developer!";
-    } else if ( $('#answer1 option:selected').val() === "ruby" ) {
-      var result = "You should be a Java developer!";
-    } else if ( $('#answer1 option:selected').val() === "java" ) {
-      var result = "You should be a Ruby developer!";
-    } else if ( $('#answer1 option:selected').val() === "php" ) {
-      var result = "You should be a PHP developer!";
+    var selected1 = $('#question1 option:selected');
+    var selected2 = $('#question2 option:selected');
+    var selected3 = $('#question3 option:selected');
+    var selected4 = $('#question4 option:selected');
+    var selected5 = $('#question5 option:selected');
+    if (selected1.val() === "c#" ) {
+      var result = "You should be a C#/.NET developer!";
+    } else if (selected1.val() === "ruby" ) {
+      var result = "You should be a Ruby/Rails developer!";
+    } else if (selected1.val() === "java" ) {
+      var result = "You should be a Java/Android developer!";
+    } else if (selected1.val() === "php" ) {
+      var result = "You should be a PHP/Drupal developer!";
+    } else if (selected1.val() === "design" ) {
+      var result = "You should study CSS/Design!";
     } else {
       var result = "Please complete the questions to see your recommended language."
     }
