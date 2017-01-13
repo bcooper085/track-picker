e7e9b4ab28d8bcd5ee12eb341082ec695e49de28
brandon <bcooper085@gmail.com>
2017-01-13 11:41:11 -0800

    add additional test questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
-var result;
-
 
 $(function() {
   $("form").submit(function() {
-    if () {
-      result ="You should be a C# developer!";
+
+    if ( $('#answer1 option:selected').val() === "c#" ) {
+      var result = "You should be a C# developer!";
+    } else if ( $('#answer1 option:selected').val() === "ruby" ) {
+      var result = "You should be a Java developer!";
+    } else if ( $('#answer1 option:selected').val() === "java" ) {
+      var result = "You should be a Ruby developer!";
+    } else if ( $('#answer1 option:selected').val() === "php" ) {
+      var result = "You should be a PHP developer!";
+    } else {
+      var result = "Please complete the questions to see your recommended language."
     }
     $("#output").text(result);
     event.preventDefault();
