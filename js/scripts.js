9d70400c1401235d3a78019a943ae6f9013fb8c4
brandon <bcooper085@gmail.com>
2017-01-13 09:33:55 -0800

    add bootstrap jumbotron, import font, css color and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
-var result; 
+var result;
 
 
 $(function() {
   $("form").submit(function() {
-
+    if () {
+      result ="You should be a C# developer!";
+    }
     $("#output").text(result);
     event.preventDefault();
   });
